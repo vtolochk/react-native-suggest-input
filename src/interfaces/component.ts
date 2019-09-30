995fec97d003b1823d90b7233e9dfe0a544933a5
Valentin Tolochko <vtolochk@gmail.com>
2019-09-30 17:35:40 +0300

    [Fix] incorrect ts props interface

--- src/interfaces/component.ts
+++ src/interfaces/component.ts
@@ -1,4 +1,11 @@
-import { TextInputProps, ViewStyle, ListRenderItem, FlatListProps, LayoutAnimationConfig } from "react-native";
+import {
+  TextInputProps,
+  ViewStyle,
+  ListRenderItem,
+  FlatListProps,
+  LayoutAnimationConfig,
+  RegisteredStyle
+} from "react-native";
 
 interface SuggestTextInput extends Partial<TextInputProps> {
   style: ViewStyle; 
@@ -7,8 +14,8 @@ interface SuggestTextInput extends Partial<TextInputProps> {
 interface SuggestInputProps {
   suggestions: any[];
   hideResults?: boolean;
-  flatListStyle?: ViewStyle;
-  containerStyle?: ViewStyle;
+  flatListStyle?: RegisteredStyle<any>;
+  containerStyle?: RegisteredStyle<any>;
   renderItem: ListRenderItem<any>;
   textInputProps: SuggestTextInput;
   flatListProps?: Partial<FlatListProps<any>>;
